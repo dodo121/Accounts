a5b2928ff5331ce6fd5e277af251e66d87e91e1b
Dominik Szromik <dodox360@gmail.com>
2016-02-27 19:17:49 +0100

    [checkpoint] finish another section
    * add dynamic adding records
    
    @dodo121

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,4 +2,20 @@ class RecordsController < ApplicationController
   def index
     @records = Record.all
   end
+
+  def create
+    @record = Record.new(records_params)
+
+    if @record.save
+      render json: @record
+    else
+      render json: @record.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def records_params
+    params.require(:record).permit(:title, :amount, :data)
+  end
 end
